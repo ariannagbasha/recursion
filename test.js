7c8c71e822347aed720d3f628a1af2c389e8b941
Arianna Basha <ariannagbasha@gmail.com>
2021-01-13 22:34:27 -0600

    intial commit, finished two functions

---
+++ test.js
@@ -0,0 +1,47 @@
+
+// // function sayHello() {
+// //     console.log("hello")
+// //     sayHello()
+// // }
+
+// // sayHello();
+
+// function countUp(num) {
+//     console.log(num)
+//     countUp(num + 1)
+// }
+
+// countUp(5)
+
+// function countUp(num) {
+
+//     `Base case`
+//     if (num = 10) {
+//         console.log("We reached the top!")
+//         return;
+//     }
+
+//     `Recursive case`
+//     console.log(num)
+//     countUp(num + 1)
+// }
+
+// countUp(5)
+
+// function fib(n) {
+//     if ( n === 1 || n === 2) {
+//         return 1;
+//     }
+//     console.log('1st Term Added',fib(n - 1))
+//     console.log('2nd Term Added',fib(n - 2))
+
+//     return fib(n - 1) + fib(n - 2);
+// }
+
+// console.log(fib(4))
+
+function sumArray(array) {
+
+}
+
+console.log(sumArray([5, 2]))
